7d2a4162085ea2a26d7cfc2700a8e648a987c1b7
Qiong (June) Hu <41148700+Qiong-Hu@users.noreply.github.com>
2019-12-11 11:45:05 -0800

    Upgrade sort_data and plot_data funcs

--- test.py
+++ test.py
@@ -93,7 +93,7 @@ def sort_data(startFrame, endFrame, clean = True):
             i_curr = i
             break
 
-    # print("i_prev=" + str(i_prev) + "\ti_curr=" + str(i_curr))
+    print("i_prev=" + str(i_prev) + "\ti_curr=" + str(i_curr))
 
     # Gaussian fit, similar to the method of voxels
     pillar = 32
@@ -101,13 +101,14 @@ def sort_data(startFrame, endFrame, clean = True):
     ver, hor = np.histogram(x[i_prev:i_curr], bins = pillar, density = True)
     mean = np.mean(x[i_prev:i_curr])
     fit = sta.norm.pdf(hor, mean, np.std(x[i_prev:i_curr]))
-    fwhm = (max(fit)+min(fit))/2
+    fwhm1 = (max(fit)+fit[0])/2
+    fwhm2 = (max(fit)+fit[-1])/2
     x_min = min(x[i_prev:i_curr])
     x_max = max(x[i_prev:i_curr])
     for i in range(len(fit)-1):
-        if fit[i] < fwhm <= fit[i+1]:
+        if fit[i] < fwhm1 <= fit[i+1]:
             x_min = hor[i+1]
-        elif fit[i] >= fwhm > fit[i+1]:
+        elif fit[i] >= fwhm2 > fit[i+1]:
             x_max = hor[i]
 
     # For y_min, y_max
@@ -129,7 +130,7 @@ def sort_data(startFrame, endFrame, clean = True):
     z_min = fit[2]-5*fit[4]
     z_max = (fit[2]+3*fit[4]+fit[3]-3*fit[5])/2  # Gap between two Gaussians
 
-    # print(x_min, x_max, y_min, y_max, z_min, z_max)
+    print(x_min, x_max, y_min, y_max, z_min, z_max)
 
     i_list = []
     for i in range(i_prev, i_curr):
@@ -143,6 +144,78 @@ def sort_data(startFrame, endFrame, clean = True):
     return i_list
     
 
+# Sorting order matters, x based on sorted z, y based on sorted x and z (z -> x -> y, because z most effective according to the preprocess, x being the next, y the least)
+# Drawbacks: two more loops, calculation heavy (probably)
+def sort_data2(startFrame, endFrame, clean = True):
+    i_prev = 0
+    for i in range(len(frame_num)):
+        if frame_num[i] == startFrame - 1:
+            i_prev = i
+            continue
+        if frame_num[i] == endFrame:
+            i_curr = i
+            break
+
+    print("i_prev=" + str(i_prev) + "\ti_curr=" + str(i_curr))
+
+    # Gaussian fit, similar to the method of voxels
+    pillar = 32
+
+    # For z_min, z_max
+    ver, hor = np.histogram(z[i_prev:i_curr], bins = pillar, density = True)
+    fit, cov = curve_fit(Gaussian2, hor[:-1], ver, p0=[1,1,0,1,0.5,0.5])
+    # plt.plot(hor, Gaussian2(hor, *fit))
+    z_min = min(z[i_prev:i_curr])
+    z_max = max(z[i_prev:i_curr])
+    z_min = fit[2]-5*fit[4]
+    z_max = (fit[2]+3*fit[4]+fit[3]-3*fit[5])/2  # Gap between two Gaussians
+
+    # For x_min, x_max
+    i_list_x = []
+    for i in range(i_prev, i_curr):
+        if z_min <= z[i] <= z_max:
+            i_list_x.append(i)
+    ver, hor = np.histogram(x[i_list_x], bins = pillar, density = True)
+    mean = np.mean(x[i_list_x])
+    fit = sta.norm.pdf(hor, mean, np.std(x[i_list_x]))
+    fwhm1 = (max(fit)+fit[0])/2
+    fwhm2 = (max(fit)+fit[-1])/2
+    x_min = min(x[i_list_x])
+    x_max = max(x[i_list_x])
+    for i in range(len(fit)-1):
+        if fit[i] < fwhm1 <= fit[i+1]:
+            x_min = hor[i+1]
+        elif fit[i] >= fwhm2 > fit[i+1]:
+            x_max = hor[i]
+
+    # For y_min, y_max
+    i_list_y = []
+    for i in range(i_prev, i_curr):
+        if x_min <= x[i] <= x_max:
+            i_list_y.append(i)
+    ver, hor = np.histogram(y[i_list_y], bins = pillar, density = True)
+    mean = np.mean(y[i_list_y])
+    std = np.std(y[i_list_y])
+    y_min = min(y[i_list_y])
+    y_max = max(y[i_list_y])
+    # print(y_min, y_max)
+    y_min = max([y_min, mean-5*std])
+    y_max = min([y_max, mean+5*std])
+
+    print(x_min, x_max, y_min, y_max, z_min, z_max)
+
+    i_list = []
+    for i in range(i_prev, i_curr):
+        if clean:
+            if x_min <= x[i] <= x_max and y_min <= y[i] <= y_max and z_min <= z[i] <= z_max:
+                i_list.append(i)
+        else:
+            i_list.append(i)
+
+    # print("i_list = " + str(i_list))
+    return i_list
+
+
 def plot_data(ax, i_list, color = 'k'):
     intensity_min = min(final[i_list, 5])
     intensity_max = max(final[i_list, 5])
@@ -154,7 +227,7 @@ def plot_data(ax, i_list, color = 'k'):
     # Frame number of the point with the max velocity (and not on the walls or ceiling)
     i_max = i_list[np.argmax(final[i_list, 4])]
     i = i_max
-    ax.scatter(x[i], y[i], z[i], color = 'r', alpha = (intensity[i] - intensity_min)/(intensity_max - intensity_min))
+    # ax.scatter(x[i], y[i], z[i], color = 'r', alpha = (intensity[i] - intensity_min)/(intensity_max - intensity_min))
     # print(i_max, x[i], y[i], z[i], (intensity[i] - intensity_min)/(intensity_max - intensity_min))
 
     ax.set_xlabel('X')
@@ -167,6 +240,7 @@ def plot_data(ax, i_list, color = 'k'):
 
 
 # Shift and add
+# Problem: if sorted data not clean enough, max velocity point may fall in noises, making the shifted data a mess
 def plot_data2(ax, i_list, color = 'k'):
     intensity_min = min(final[i_list, 5])
     intensity_max = max(final[i_list, 5])
@@ -187,9 +261,9 @@ def plot_data2(ax, i_list, color = 'k'):
         vel_max_frame_index = data_new[each_frame][np.argmax(velocity[data_new[each_frame]])]
         for i in data_new[each_frame]:
             ax.scatter(x[i]-(x[vel_max_frame_index]-x[i_max]), y[i]-(y[vel_max_frame_index]-y[i_max]), z[i]-(z[vel_max_frame_index]-z[i_max]), color = color, alpha = (intensity[i] - intensity_min)/(intensity_max - intensity_min))
-    i = i_max
-    ax.scatter(x[i], y[i], z[i], color = 'r', alpha = (intensity[i] - intensity_min)/(intensity_max - intensity_min))
-    print(i_max, x[i], y[i], z[i], (intensity[i] - intensity_min)/(intensity_max - intensity_min))
+    # i = i_max
+    # ax.scatter(x[i], y[i], z[i], color = 'r', alpha = (intensity[i] - intensity_min)/(intensity_max - intensity_min))
+    # print(i_max, x[i], y[i], z[i], (intensity[i] - intensity_min)/(intensity_max - intensity_min))
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
@@ -200,7 +274,14 @@ def plot_data2(ax, i_list, color = 'k'):
     # plt.show()
 
 
-def plot_cube(ax, i_list, alpha = 1, color = 'r'):
+def expect(i_list):
+    x = np.dot(final[i_list, 1], final[i_list, 4])/np.sum(final[i_list, 4])
+    y = np.dot(final[i_list, 2], final[i_list, 4])/np.sum(final[i_list, 4])
+    z = np.dot(final[i_list, 3], final[i_list, 4])/np.sum(final[i_list, 4])
+    return [x, y, z]
+
+
+def find_verts(i_list):
     x_min = min(final[i_list, 1])
     x_max = max(final[i_list, 1])
     y_min = min(final[i_list, 2])
@@ -212,6 +293,16 @@ def plot_cube(ax, i_list, alpha = 1, color = 'r'):
     intensity_min = min(final[i_list, 5])
     intensity_max = max(final[i_list, 5])
 
+    center = expect(i_list)
+
+    verts = [x_min, x_max, y_min, y_max, z_min, z_max, center]
+
+    return verts
+
+
+def plot_cube(ax, verts, alpha = 1, color = 'r'):
+    x_min, x_max, y_min, y_max, z_min, z_max, center = verts
+
     xx = [x_min, x_min, x_max, x_max, x_min]
     yy = [y_min, y_max, y_max, y_min, y_min]
     kwargs = {'alpha': alpha, 'color': color}
@@ -221,6 +312,13 @@ def plot_cube(ax, i_list, alpha = 1, color = 'r'):
     ax.plot3D([x_min, x_min], [y_max, y_max], [z_min, z_max], **kwargs)
     ax.plot3D([x_max, x_max], [y_min, y_min], [z_min, z_max], **kwargs)
     ax.plot3D([x_max, x_max], [y_max, y_max], [z_min, z_max], **kwargs)
+
+    # Estimate the center point of the figure
+    # Use the point with max velocity as the estimated center
+    # i_max = i_list[np.argmax(final[i_list, 4])]     
+    # ax.scatter(x[i_max], y[i_max], z[i_max], color = 'r')
+    # Use the velocity-weighted expected position as the estimated center
+    ax.scatter(center[0], center[1], center[2], color = 'r')
 
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
@@ -238,11 +336,14 @@ data, final = organized_data()
 
 fig = plt.figure()
 ax = fig.gca(projection = '3d')
-i_list = sort_data(0, 60)
-# plot_cube(ax, i_list, color='y')
+i_list = sort_data2(120, 130)
 plot_data(ax, i_list)
+verts = find_verts(i_list)
+plot_cube(ax, verts)
 
+
+# fig = plt.figure()
+# ax = fig.gca(projection = '3d')
+# i_list = sort_data2(180, 240, False)
+# plot_data(ax, i_list)
 plt.show()
-
-
-
